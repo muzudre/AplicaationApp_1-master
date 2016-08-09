7f5e3651bcc134bea48a59c7bba4c281e5a640a2
muzudre <muzudre@gmail.com>
2016-08-09 15:24:22 +0800

    new

--- app/src/main/java/com/example/muzud/aplicaationapp_1/Lectures.java
+++ app/src/main/java/com/example/muzud/aplicaationapp_1/Lectures.java
@@ -1,11 +1,14 @@
 package com.example.muzud.aplicaationapp_1;
 
 import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.view.MenuItem;
 import android.webkit.WebView;
@@ -19,29 +22,18 @@ import java.util.ArrayList;
 public class Lectures extends AppCompatActivity {
 
     ActionBarDrawerToggle toggle;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lectures);
 
-        String url = "file:///android_asset/new/lectures.html";
-        WebView view = (WebView)findViewById(R.id.webView3);
-        view.getSettings().setJavaScriptEnabled(true);
-        view.loadUrl(url);
-        view.setWebViewClient(new MyBrowser());
 
 
 
-    }
 
-    private class MyBrowser extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url){
-            view.loadUrl(url);
-            return true;
-        }
-    }
+  }
 
 
 }
